d652308de7595b83ecb21ed87e14fd3589efa9bd
JoshuaOliver99 <joshuaoliversimpson@gmail.com>
2023-02-02 10:45:47 +0000

    Completed " Polish And Wrap-Up"

--- ToonTanks/Source/ToonTanks/BasePawn.cpp
+++ ToonTanks/Source/ToonTanks/BasePawn.cpp
@@ -41,7 +41,7 @@ void ABasePawn::RotateTurret(FVector LookAtTarget)
 
 void ABasePawn::Fire()
 {
-	const auto Projectile = GetWorld()->SpawnActor<AProjectile>(
+	AProjectile* Projectile = GetWorld()->SpawnActor<AProjectile>(
 		ProjectileClass ,
 		ProjectileSpawnPoint->GetComponentLocation(),
 		ProjectileSpawnPoint->GetComponentRotation());
